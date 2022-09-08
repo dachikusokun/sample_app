7d77b7a7e3a7b5625594bed3ae141bbf95158ced
dachikusokun <ardija.xyz@gmail.com>
2022-09-08 07:29:25 +0000

    cheng host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'vast-hamlet-50689.herokuapp.com'
+  host = 'https://vast-hamlet-50689.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
